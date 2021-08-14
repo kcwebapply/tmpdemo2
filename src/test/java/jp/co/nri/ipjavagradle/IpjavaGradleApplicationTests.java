51b136923f7d28a4abef788dcac7c6c7328b5c8c
kcaiueo <kcwebapply@gmail.com>
2021-08-14 15:39:33 +0900

    fix

---
+++ src/test/java/jp/co/nri/ipjavagradle/IpjavaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.nri.ipjavagradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpjavaGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
